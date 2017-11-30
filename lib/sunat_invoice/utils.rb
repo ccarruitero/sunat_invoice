d65810898791da4c0ca0ad5dcc1398d0f72cbe10
Cesar Carruitero <cesar@mozilla.pe>
2017-11-30 02:57:40 -0500

    cleanup utils

--- lib/sunat_invoice/utils.rb
+++ lib/sunat_invoice/utils.rb
@@ -1,102 +1,22 @@
 # frozen_string_literal: false
 module Utils
-  @namespace_path = 'urn:oasis:names:specification:ubl:schema:xsd:'
+  @namespace_path = 'urn:oasis:names:specification:ubl:schema:xsd'
+  @sunat_namespace_path = 'urn:sunat:names:specification:ubl:peru:schema:xsd'
+
   UBL_NAMESPACES = {
-    'xmlns' => @namespace_path + 'Invoice-2',
-    'xmlns:cac' => @namespace_path + 'CommonAggregateComponents-2',
-    'xmlns:cbc' => @namespace_path + 'CommonBasicComponents-2',
-    'xmlns:ext' => @namespace_path + 'CommonExtensionComponents-2',
-    'xmlns:ds' => 'http://www.w3.org/2000/09/xmldsig#'
+    'xmlns' => "#{@namespace_path}:Invoice-2",
+    'xmlns:cac' => "#{@namespace_path}:CommonAggregateComponents-2",
+    'xmlns:cbc' => "#{@namespace_path}:CommonBasicComponents-2",
+    'xmlns:ext' => "#{@namespace_path}:CommonExtensionComponents-2",
+    'xmlns:ds' => 'http://www.w3.org/2000/09/xmldsig#',
+    'xmlns:sac' => "#{@sunat_namespace_path}:SunatAggregateComponents-1",
+    'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+    'xsi:schemaLocation' => 'urn:sunat:names:specification:ubl:peru:schema:xsd:InvoiceSummary-1 D:\UBL_SUNAT\SUNAT_xml_20110112\20110112\xsd\maindoc\UBLPE-InvoiceSummary-1.0.xsd'
   }.freeze
 
-  def regex_type(mode, key)
-    case mode
-
-    when 'after'
-      "/#{key}"
-    else
-      key
+  def ubl_ext(xml, &block)
+    xml['ext'].UBLExtension do
+      xml['ext'].ExtensionContent(&block)
     end
-  end
-
-  def _index(mode, key)
-    case mode
-
-    when 'after'
-      key.to_s.length + 2
-    when 'before'
-      - 1
-    when 'inside'
-      key.to_s.length + 1
-    end
-  end
-
-  def concat_xml(parent_str, child_str, key, type = nil)
-    return unless parent_str.is_a?(String) && child_str.is_a?(String)
-    mode = type || 'after'
-    i = parent_str.index(regex_type(mode, key)) + _index(mode, key)
-
-    parent_str.insert(i, child_str)
-  end
-
-  def ubl_ext(hash)
-    {
-      'ext:UBLExtensions' => {
-        'ext:UBLExtension' => {
-          'ext:ExtensionContent' => hash
-        }
-      }
-    }
-  end
-
-  def tax_scheme(amount, id, name, tax_type)
-    {
-      'cac:TaxTotal' => {
-        'cbc:TaxAmount' => amount,
-        'cac:TaxSubtotal' => {
-          'cbc:TaxAmount' => amount,
-          'cac:TaxCategory' => {
-            'cac:TaxScheme' => {
-              'cbc:ID' => id,
-              'cbc:Name' => name,
-              'cbc:TaxTypeCode' => tax_type
-            }
-          }
-        }
-      }
-    }
-  end
-
-  def igv_tax(igv_amount)
-    main_xml = Gyoku.xml(tax_scheme(igv_amount, 1000, 'IGV', 'VAT'))
-    child_xml = Gyoku.xml('cbc:TaxExemptionReasonCode' => '10')
-    concat_xml(main_xml, child_xml, 'cac:TaxCategory', 'inside')
-  end
-
-  def isc_tax(isc_amount)
-    main_xml = Gyoku.xml(tax_scheme(isc_amount, 2000, 'ISC', 'EXC'))
-    child_xml = Gyoku.xml('cbc:TierRange' => '02')
-    concat_xml(main_xml, child_xml, 'cac:TaxCategory', 'inside')
-  end
-
-  def other_tax(other_amount)
-    Gyoku.xml(other_amount, 9999, 'OTROS', 'OTH')
-  end
-
-  def total_tax(venta_gravada, venta_inafecta, venta_exonerada, venta_gratuita)
-    str = ''
-    str << payable_xml(venta_gravada, 1001)
-    str << payable_xml(venta_inafecta, 1002)
-    str << payable_xml(venta_exonerada, 1003)
-    str << payable_xml(venta_gratuita, 1004)
-  end
-
-  private
-
-  def payable_xml(amount, code)
-    Gyoku.xml('sac:AdditionalMonetaryTotal' => {
-                'cbc:ID' => code,
-                'cbc:PayableAmount' => amount
-              })
   end
 end
