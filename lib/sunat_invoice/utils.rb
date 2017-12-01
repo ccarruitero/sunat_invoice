e734a8de61ef7e5a19c22ab7fd19297b23139537
Cesar Carruitero <cesar@mozilla.pe>
2017-12-01 02:24:31 -0500

    namespace Utils

--- lib/sunat_invoice/utils.rb
+++ lib/sunat_invoice/utils.rb
@@ -1,22 +1,25 @@
 # frozen_string_literal: false
-module Utils
-  @namespace_path = 'urn:oasis:names:specification:ubl:schema:xsd'
-  @sunat_namespace_path = 'urn:sunat:names:specification:ubl:peru:schema:xsd'
 
-  UBL_NAMESPACES = {
-    'xmlns' => "#{@namespace_path}:Invoice-2",
-    'xmlns:cac' => "#{@namespace_path}:CommonAggregateComponents-2",
-    'xmlns:cbc' => "#{@namespace_path}:CommonBasicComponents-2",
-    'xmlns:ext' => "#{@namespace_path}:CommonExtensionComponents-2",
-    'xmlns:ds' => 'http://www.w3.org/2000/09/xmldsig#',
-    'xmlns:sac' => "#{@sunat_namespace_path}:SunatAggregateComponents-1",
-    'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
-    'xsi:schemaLocation' => 'urn:sunat:names:specification:ubl:peru:schema:xsd:InvoiceSummary-1 D:\UBL_SUNAT\SUNAT_xml_20110112\20110112\xsd\maindoc\UBLPE-InvoiceSummary-1.0.xsd'
-  }.freeze
+module SunatInvoice
+  module Utils
+    @namespace_path = 'urn:oasis:names:specification:ubl:schema:xsd'
+    @sunat_namespace_path = 'urn:sunat:names:specification:ubl:peru:schema:xsd'
 
-  def ubl_ext(xml, &block)
-    xml['ext'].UBLExtension do
-      xml['ext'].ExtensionContent(&block)
+    UBL_NAMESPACES = {
+      'xmlns' => "#{@namespace_path}:Invoice-2",
+      'xmlns:cac' => "#{@namespace_path}:CommonAggregateComponents-2",
+      'xmlns:cbc' => "#{@namespace_path}:CommonBasicComponents-2",
+      'xmlns:ext' => "#{@namespace_path}:CommonExtensionComponents-2",
+      'xmlns:ds' => 'http://www.w3.org/2000/09/xmldsig#',
+      'xmlns:sac' => "#{@sunat_namespace_path}:SunatAggregateComponents-1",
+      'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+      'xsi:schemaLocation' => 'urn:sunat:names:specification:ubl:peru:schema:xsd:InvoiceSummary-1 D:\UBL_SUNAT\SUNAT_xml_20110112\20110112\xsd\maindoc\UBLPE-InvoiceSummary-1.0.xsd'
+    }.freeze
+
+    def ubl_ext(xml, &block)
+      xml['ext'].UBLExtension do
+        xml['ext'].ExtensionContent(&block)
+      end
     end
   end
 end
