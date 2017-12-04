63354dbe67727d187c499ff2ac82e17090ad7835
Cesar Carruitero <cesar@mozilla.pe>
2017-12-04 00:33:15 -0500

    cleanup unused methods in client

--- lib/sunat_invoice/client.rb
+++ lib/sunat_invoice/client.rb
@@ -30,17 +30,7 @@ module SunatInvoice
       Savon.client(wsdl: wsdl,
                    wsse_auth: authentication,
                    namespace: 'http://service.sunat.gob.pe',
-                   ssl_cert_file: cert_file,
-                   log: log,
-                   ssl_cert_key_file: pk_file)
-    end
-
-    def cert_file
-      config.provider.cert_file
-    end
-
-    def pk_file
-      config.provider.pk_file
+                   log: log)
     end
   end
 end
