5a14ae63131f40adddea55cf4141d513ae6fa28d
Cesar Carruitero <cesar@mozilla.pe>
2017-12-06 21:51:58 -0500

    add operations for ConsultClient

--- lib/sunat_invoice.rb
+++ lib/sunat_invoice.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
+# documents
 require 'sunat_invoice/xml_document'
 require 'sunat_invoice/invoice'
 require 'sunat_invoice/provider'
 require 'sunat_invoice/customer'
 require 'sunat_invoice/item'
 require 'sunat_invoice/tax'
+require 'sunat_invoice/daily_summary'
+require 'sunat_invoice/summary_line'
+
+# clients
 require 'sunat_invoice/configuration'
 require 'sunat_invoice/response_parser'
 require 'sunat_invoice/client'
 require 'sunat_invoice/clients/invoice_client'
-require 'sunat_invoice/daily_summary'
-require 'sunat_invoice/summary_line'
+require 'sunat_invoice/clients/consult_client'
 
 module SunatInvoice
   class << self
--- lib/sunat_invoice/clients/consult_client.rb
+++ lib/sunat_invoice/clients/consult_client.rb
@@ -7,5 +7,22 @@ module SunatInvoice
     def wsdl
       'https://www.sunat.gob.pe/ol-it-wsconscpegem/billConsultService?wsdl'
     end
+
+    def get_status(ticket)
+      @soap_client.call(:get_status, message: { ticket: ticket })
+    end
+
+    def get_status_cdr(options = {})
+      # Available document_type to use:
+      #   01: Factura.
+      #   07: Nota de crédito.
+      #   08: Nota de débito
+      @soap_client.call(:get_status, message: {
+                          rucComprobante: options[:ruc],
+                          tipoComprobante: options[:document_type],
+                          serieComprobante: options[:document_serial],
+                          numeroComprobante: options[:document_number]
+                       })
+    end
   end
 end
