b83328fa76e1f0a18973a641e2de08f468aa0606
César Carruitero <ccarruitero@protonmail.com>
2019-12-23 16:02:39 -0500

    add to_hash method to SunatInvoice::Model

--- lib/sunat_invoice/model.rb
+++ lib/sunat_invoice/model.rb
@@ -7,5 +7,14 @@ module SunatInvoice
         send("#{key}=", value) if respond_to?("#{key}=")
       end
     end
+
+    def to_hash
+      hash = {}
+      instance_variables.map do |var|
+        var_name = var.to_s.split('@')[1]
+        hash[var_name] = instance_variable_get(var)
+      end
+      hash
+    end
   end
 end
